c261bc73d96d6bba767c54c86957b89b6a8869df
olejaaaaaaaa <olejav7@gmail.com>
2026-02-06 05:48:55 +0300

    fix format for linux

--- src/render_graph/texture/linux.rs
+++ src/render_graph/texture/linux.rs
@@ -5,9 +5,20 @@
 // 87.32%	12.68% A2R10G10B10_UNORM_PACK32	    SRGB_NONLINEAR_KHR
 // 28.06%	71.94%
 
+// pub enum TextureFormat {
+//     D32Sfloat,
+//     B8G8R8A8_UNORM,
+//     B8G8R8A8_SRGB,
+//     A2R10G10B10_UNORM_PACK32,
+// }
+
+/// Formats for Texture
+#[derive(Debug)]
 pub enum TextureFormat {
+    /// For Depth
     D32Sfloat,
-    B8G8R8A8_UNORM,
-    B8G8R8A8_SRGB,
-    A2R10G10B10_UNORM_PACK32,
+    /// For Image
+    R8g8b8a8Srgb,
+    /// For Image
+    R8g8b8a8Unorm,
 }
