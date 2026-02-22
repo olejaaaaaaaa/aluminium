0a06e5c92f2cd033bc5f2625dd6819fd85f03440
olejaaaaaaaa <olejav7@gmail.com>
2026-02-22 12:22:37 +0300

    add vendor ids

---
+++ src/core/vendor.rs
@@ -0,0 +1,16 @@
+/// Known PCI vendor IDs for GPU manufacturers
+/// 
+/// List of known IDs referenced from Godot Engine source:
+/// 
+/// <https://github.com/godotengine/godot/blob/a3e84cc2af14aa4cffbefd8e13492e567a64e3/servers/rendering/rendering_context_driver.h>
+pub enum Vendor {
+    Unknown = 0x0,
+    Amd = 0x1002,
+    Imgtec = 0x1010,
+    Apple = 0x106B,
+    Nvidia = 0x10DE,
+    Arm = 0x13B5,
+    Microsoft = 0x1414,
+    Qualcomm = 0x5143,
+    Intel = 0x8086,
+}
